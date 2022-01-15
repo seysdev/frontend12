43699757a3ab1ec5a59fd31b06a0746f0fb4ac91
sebastian yabiku <sby2710@gmail.com>
2022-01-14 20:01:16 -0500

    se agrego objetos

--- semana-5/viernes/script/main.js
+++ semana-5/viernes/script/main.js
@@ -71,3 +71,90 @@ alumnos.setAlumno({
 console.log("alumno jose", alumnos.getAlumno("jose"));
 console.log("alumno melisa", alumnos.getAlumno("melisa"));
 console.log("alumnos", alumnos);
+
+/*
+nombre 
+detalle
+precio
+marca
+numero de serie
+anio
+procedencia
+*/
+
+let iphone = {
+  nombre: "iphone",
+  modelo: 11,
+  marca: "apple",
+  numeroSerie: 1234,
+  procendencia: "usa",
+  getInfo: function () {
+    return `El nombre del producto es ${this.nombre} el modelo es ${this.modelo} de la marca ${this.marca}`;
+  },
+};
+
+let android = {
+  nombre: "android",
+  modelo: 12,
+  marca: "huawey",
+  numeroSerie: 1234,
+  procendencia: "usa",
+  getInfo: function () {
+    return `El nombre del producto es ${this.nombre} el modelo es ${this.modelo} de la marca ${this.marca}`;
+  },
+};
+
+let huawey = {
+  nombre: "huawey",
+  modelo: 12,
+  marca: "abc",
+  numeroSerie: 1234,
+  procendencia: "usa",
+  getInfo: function () {
+    return `El nombre del producto es ${this.nombre} el modelo es ${this.modelo} de la marca ${this.marca}`;
+  },
+};
+
+let firefox = new Object(); // {}
+firefox.nombre = "firefox";
+firefox.modelo = "firefox";
+firefox.marca = "apple";
+firefox.numeroSerie = 1234;
+firefox.procendencia = "usa";
+firefox.getInfo = function () {
+  return `El nombre del producto es ${this.nombre} el modelo es ${this.modelo} de la marca ${this.marca}`;
+};
+
+/*CONSTRUCTOR DE OBJETOS */
+function makeMobile(props) {
+  this.nombre = props.nombre;
+  this.modelo = props.modelo;
+  this.marca = props.marca;
+  this.numeroSerie = props.numeroSerie;
+  this.procedencia = props.procedencia;
+  this.getInfo = function () {
+    return `El nombre del producto es ${this.nombre} el modelo es ${this.modelo} de la marca ${this.marca}`;
+  };
+}
+
+let celularUno = new makeMobile({
+  nombre: "celularUno",
+  modelo: "modeloUno",
+  marca: "marcaUno",
+  numeroSerie: "unoserie",
+  procedencia: "china",
+});
+
+console.log("celularUno", celularUno.getInfo());
+
+let celuLarDos = new makeMobile({
+  nombre: "celulardos",
+  modelo: "modelodos",
+  marca: "marcados",
+  numeroSerie: "dosserie",
+  procedencia: "china",
+});
+
+console.log("celuLarDos", celuLarDos.getInfo());
+
+
