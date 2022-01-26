7518986c359e959de177b5f54a3fe2c1f08d447a
sebastian yabiku <sby2710@gmail.com>
2022-01-25 19:56:23 -0500

    cambios

--- semana-7/martes/scripts/ejercicios.js
+++ semana-7/martes/scripts/ejercicios.js
@@ -6,9 +6,25 @@ fetch("https://61ef3d0cd593d20017dbb393.mockapi.io/computers")
   .then(function (computers) {
     console.log("computers", computers);
     computers.forEach(function (computer) {
-      console.log("computer", computer);
+      agregarNodoEnHtml(crearNodo(computer));
     });
   });
+
+function crearNodo(computer) {
+  console.log("makeComputer", computer);
+  const element = document.createElement("article");
+  element.innerHTML = `
+    ${computer.marca}
+  `;
+
+  return element;
+}
+
+function agregarNodoEnHtml(dom) {
+  const app = document.getElementById("app");
+
+  app.appendChild(dom);
+}
 
 console.log("1");
 console.log("2");
@@ -21,3 +37,23 @@ GET
 Usando el metodo get 
 realizar una peticion al endpoint creado con mockapi y pintar los datos en el navegador
 */
+let datos = [];
+function obtenerDatos(callback) {
+  fetch("https://61ef3d0cd593d20017dbb393.mockapi.io/computers")
+    .then((response) => {
+      return response.json();
+    })
+    .then((respuesta) => {
+      callback(respuesta);
+      datos = respuesta;
+    });
+}
+
+obtenerDatos(function (respuesta) {
+  console.log("respuesta", respuesta);
+});
+console.log("datos", datos);
+
+obtenerDatos(function (respuesta) {
+  console.log("respuesta", respuesta);
+});
