eac5982aa4dd6a8c2a8c279f410c0b8b029f0d55
sebastian yabiku <sby2710@gmail.com>
2022-01-25 21:37:45 -0500

    codigo asincrono

--- semana-7/martes/scripts/ejercicios.js
+++ semana-7/martes/scripts/ejercicios.js
@@ -1,57 +1,79 @@
 // GET - Se hace la peticion
-fetch("https://61ef3d0cd593d20017dbb393.mockapi.io/computers")
-  .then(function (respuesta) {
-    return respuesta.json();
-  })
-  .then(function (computers) {
-    computers.forEach(function (computadora) { // se recorre coleccion
-      agregarNodoEnHtml(crearNodo(computadora)); 
+function obtenerComputadoras() {
+  fetch("https://61ef3d0cd593d20017dbb393.mockapi.io/computers")
+    .then(function (respuesta) {
+      return respuesta.json();
+    })
+    .then(function (computers) {
+      document.getElementById("app").innerHTML = "";
+      computers.forEach(function (computadora) {
+        // se recorre coleccion
+        agregarNodoEnHtml(crearNodo(computadora));
+      });
     });
-  });
 
-function crearNodo(computadora) {
-  const nodo = document.createElement("article");
-  nodo.innerHTML = `
-    <article class="card mb-4">
-        <div class="card-body">
-            <h5 class="card-title">Marca: ${computadora.marca}</h5>
-            <p class="card-text">Memoria: ${computadora.memoria}</p>
-            <p class="card-text">Tamanio: ${computadora.tamanio}</p>
-        </div>
-      </article>
-  `;
+  function crearNodo(computadora) {
+    const nodo = document.createElement("article");
+    nodo.innerHTML = `
+      <article class="card mb-4">
+          <div class="card-body">
+              <h5  class="card-title">Marca: ${computadora.marca}</h5>
+              <p class="card-text">Memoria: ${computadora.memoria}</p>
+              <p class="card-text">Tamanio: ${computadora.tamanio}</p>
+          </div>
+        </article>
+    `;
 
-  return nodo;
+    return nodo;
+  }
+
+  function agregarNodoEnHtml(nodo) {
+    const app = document.getElementById("app");
+    app.appendChild(nodo);
+  }
 }
-
-function agregarNodoEnHtml(nodo) {
-  const app = document.getElementById("app");
-  app.appendChild(nodo);
-}
-
 
 /*
 GET
 Usando el metodo get 
 realizar una peticion al endpoint creado con mockapi y pintar los datos en el navegador
 */
-let datos = [];
-function obtenerDatos(callback) {
-  fetch("https://61ef3d0cd593d20017dbb393.mockapi.io/computers")
-    .then((response) => {
-      return response.json();
+// obtenerComputadoras();
+
+/*POST */
+function guardarComputadora() {
+  const form = document.querySelector("form");
+
+  function obtenerValores() {
+    return {
+      marca: document.querySelector(".js_marca").value,
+      memoria: document.querySelector(".js_memoria").value,
+      color: document.querySelector(".js_color").value,
+      photo: document.querySelector(".js_photo").value,
+    };
+  }
+
+  form.onsubmit = function (event) {
+    event.preventDefault();
+
+    fetch("https://61ef3d0cd593d20017dbb393.mockapi.io/computers", {
+      method: "POST",
+      body: JSON.stringify(obtenerValores()),
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .then((respuesta) => {
-      callback(respuesta);
-      datos = respuesta;
-    });
+      .then((respuesta) => respuesta.json())
+      .then((respuesta) => {
+        console.log("respuesta", respuesta);
+        obtenerComputadoras();
+      })
+      .catch((error) => console.log("error", error));
+    //   .then((respuesta) => console.log(respuesta));
+    // .then(function(respuesta) {
+    //     return respuesta.json()
+    // })
+  };
 }
-
-obtenerDatos(function (respuesta) {
-  console.log("respuesta", respuesta);
-});
-console.log("datos", datos);
-
-obtenerDatos(function (respuesta) {
-  console.log("respuesta", respuesta);
-});
+obtenerComputadoras();
+guardarComputadora();
