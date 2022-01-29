dff558d3c24dafcc15de27a13a6602ed6bc63aa2
sebastian yabiku <sby2710@gmail.com>
2022-01-28 20:18:26 -0500

    proyectos react

---
+++ semana-7/viernes/script/main.js
@@ -0,0 +1,5 @@
+import { sumar, restar, multiplicar } from "./operations.js";
+
+console.log("sumar", sumar(2, 3));
+console.log("restar", restar(4, 3));
+console.log("multiplicar", multiplicar(4, 3));
---
+++ semana-7/viernes/script/operations.js
@@ -0,0 +1,11 @@
+export function sumar(a, b) {
+  return a + b;
+}
+
+export function restar(a, b) {
+  return a - b;
+}
+
+export function multiplicar(a, b) {
+  return a * b;
+}
